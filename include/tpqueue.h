5acba7525987fbb66a2ecb8bbe48d4054061103e
Джалоладдин <joniatabekovich07@gmail.com>
2026-05-23 19:30:16 +0300

    tpqueue.h

--- include/tpqueue.h
+++ include/tpqueue.h
@@ -1,15 +1,69 @@
 // Copyright 2022 NNTU-CS
-#ifndef INCLUDE_TPQUEUE_H_
-#define INCLUDE_TPQUEUE_H_
-
-template<typename T>
-class TPQueue {
-  // реализация шаблона очереди с приоритетом на связанном списке
-};
+#pragma once
 
 struct SYM {
-  char ch;
-  int prior;
+    char ch;
+    int prior;
 };
 
-#endif  // INCLUDE_TPQUEUE_H_
+template <typename T>
+class TPQueue {
+ private:
+    struct Node {
+        T data;
+        Node* next;
+
+        Node(const T& value) {
+            data = value;
+            next = nullptr;
+        }
+    };
+
+    Node* head;
+
+ public:
+    TPQueue() {
+        head = nullptr;
+    }
+
+    ~TPQueue() {
+        while (head != nullptr) {
+            Node* temp = head;
+            head = head->next;
+            delete temp;
+        }
+    }
+
+    void push(const T& value) {
+        Node* newNode = new Node(value);
+
+        // boshiga qo'shish
+        if (head == nullptr || value.prior > head->data.prior) {
+            newNode->next = head;
+            head = newNode;
+            return;
+        }
+
+        Node* current = head;
+
+        // FIFO uchun >= kerak
+        while (current->next != nullptr &&
+               current->next->data.prior >= value.prior) {
+            current = current->next;
+        }
+
+        newNode->next = current->next;
+        current->next = newNode;
+    }
+
+    T pop() {
+        Node* temp = head;
+        T value = head->data;
+
+        head = head->next;
+
+        delete temp;
+
+        return value;
+    }
+};
