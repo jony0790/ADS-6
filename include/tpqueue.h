224922efe3212ae1e67c5c6c4157e81f880192e9
Джалоладдин <joniatabekovich07@gmail.com>
2026-06-05 19:51:53 +0300

    Джумабаев Джалоладдин 25-ИСТ-1-1

--- include/tpqueue.h
+++ include/tpqueue.h
@@ -1,15 +1,70 @@
-// Copyright 2022 NNTU-CS
+// Copyright 2026
+
 #ifndef INCLUDE_TPQUEUE_H_
 #define INCLUDE_TPQUEUE_H_
 
-template<typename T>
-class TPQueue {
-  // реализация шаблона очереди с приоритетом на связанном списке
+struct SYM {
+    char ch;
+    int prior;
 };
 
-struct SYM {
-  char ch;
-  int prior;
+template <class T>
+class TPQueue {
+ private:
+    struct Node {
+        T data;
+        Node* next;
+
+        explicit Node(const T& value)
+            : data(value), next(nullptr) {
+        }
+    };
+
+    Node* head;
+
+ public:
+    TPQueue()
+        : head(nullptr) {
+    }
+
+    ~TPQueue() {
+        while (head != nullptr) {
+            Node* tmp = head;
+            head = head->next;
+            delete tmp;
+        }
+    }
+
+    void push(const T& value) {
+        Node* node = new Node(value);
+
+        if (head == nullptr ||
+            value.prior > head->data.prior) {
+            node->next = head;
+            head = node;
+            return;
+        }
+
+        Node* cur = head;
+
+        while (cur->next != nullptr &&
+               cur->next->data.prior >= value.prior) {
+            cur = cur->next;
+        }
+
+        node->next = cur->next;
+        cur->next = node;
+    }
+
+    T pop() {
+        Node* tmp = head;
+        T value = head->data;
+
+        head = head->next;
+        delete tmp;
+
+        return value;
+    }
 };
 
 #endif  // INCLUDE_TPQUEUE_H_
